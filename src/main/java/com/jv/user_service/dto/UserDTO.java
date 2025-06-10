ff88cdadc6e3aa6c9be254e35b35017c12955ab2
Your Name <joaovgbv@gmail.com>
2025-06-10 12:12:21 -0300

    Criação UserDTO

---
+++ src/main/java/com/jv/user_service/dto/UserDTO.java
@@ -0,0 +1,51 @@
+package com.jv.user_service.dto;
+
+import java.util.Date;
+
+public class UserDTO {
+    private String nome;
+    private String cpf;
+    private String endereco;
+    private String telefone;
+    private Date dataCadastro;
+
+    public Date getDataCadastro() {
+        return dataCadastro;
+    }
+
+    public void setDataCadastro(Date dataCadastro) {
+        this.dataCadastro = dataCadastro;
+    }
+
+    public String getTelefone() {
+        return telefone;
+    }
+
+    public void setTelefone(String telefone) {
+        this.telefone = telefone;
+    }
+
+    public String getEndereco() {
+        return endereco;
+    }
+
+    public void setEndereco(String endereco) {
+        this.endereco = endereco;
+    }
+
+    public String getCpf() {
+        return cpf;
+    }
+
+    public void setCpf(String cpf) {
+        this.cpf = cpf;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+}
