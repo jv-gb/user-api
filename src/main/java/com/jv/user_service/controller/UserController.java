bd4d0d338a5a5c1617047395961bc9c1768c5ae8
Your Name <joaovgbv@gmail.com>
2025-06-12 10:39:51 -0300

    Criação da rota /users

--- src/main/java/com/jv/user_service/controller/UserController.java
+++ src/main/java/com/jv/user_service/controller/UserController.java
@@ -1,7 +1,12 @@
 package com.jv.user_service.controller;
 
+import com.jv.user_service.dto.UserDTO;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+import static com.jv.user_service.services.UserServices.usuarios;
 
 @RestController
 public class UserController {
@@ -10,5 +15,8 @@ public class UserController {
         return "Está funcionando!";
     }
 
-
+    @GetMapping("/users")
+public List<UserDTO> getUsers(){
+        return usuarios;
+}
 }
