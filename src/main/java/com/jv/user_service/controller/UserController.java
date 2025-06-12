7fea072033003858357c8e82acd8f7ce443f22e0
Your Name <joaovgbv@gmail.com>
2025-06-12 11:23:06 -0300

    Criação getUserFiltro

--- src/main/java/com/jv/user_service/controller/UserController.java
+++ src/main/java/com/jv/user_service/controller/UserController.java
@@ -2,6 +2,7 @@ package com.jv.user_service.controller;
 
 import com.jv.user_service.dto.UserDTO;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
@@ -11,12 +12,23 @@ import static com.jv.user_service.services.UserServices.usuarios;
 @RestController
 public class UserController {
     @GetMapping("/")
-    public String getMensagem(){
+    public String getMensagem() {
         return "Está funcionando!";
     }
 
     @GetMapping("/users")
-public List<UserDTO> getUsers(){
+    public List<UserDTO> getUsers() {
         return usuarios;
-}
+    }
+
+    @GetMapping("/users/{cpf}")
+    public UserDTO getUserFiltro(@PathVariable String cpf) {
+        for (UserDTO userFilter : usuarios) {
+            if (userFilter.getCpf().equals(cpf)) {
+                return userFilter;
+            }
+
+        }
+        return null;
+    }
 }
